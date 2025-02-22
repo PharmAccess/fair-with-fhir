230018911710e8a83df6b14a4d0a021380de1202
Daniel Kapitan <daniel@kapitan.net>
2025-02-22 11:13:18 +0100

    Cleanup external files

--- data/external/README.md
+++ data/external/README.md
@@ -5,3 +5,10 @@
     - FHIR R6 has more up-to-date FHIR ontology, but since we use profiles based on R4, we choose this version
 - SnomedCT IPS Terminology: https://www.snomed.org/international-patient-summary-terminology
     - Need to register and download manually
+- ICD-10 2019: https://icdcdn.who.int/icd10/index.html
+
+## Not used for reference
+
+- UMLS Knowledge Sources
+    - https://www.nlm.nih.gov/research/umls/licensedcontent/umlsknowledgesources.html, three different files
+-  [Semantic Network](https://www.nlm.nih.gov/research/umls/knowledge_sources/semantic_network/index.html#label:download): `sn_current.tgz`
---
+++ who-anc-sct-errors.py
@@ -0,0 +1,124 @@
+# SNOMED-CT codes with exponent notation
+sct_errors = [
+    {
+        "code": "ANC.B5.DE24",
+        "display": "Leg cramps",
+        "target": [
+            {
+                "code": "1.56399E+16",
+                "display": "Cramp of muscle of bilateral lower limbs (finding)",
+                "equivalence": "relatedto",
+            }
+        ],
+    },
+    {
+        "code": "ANC.B5.DE33",
+        "display": "Pain - Leg",
+        "target": [
+            {
+                "code": "1.22475E+16",
+                "display": "Pain in bilateral lower legs (finding)",
+                "equivalence": "relatedto",
+            }
+        ],
+    },
+    {
+        "code": "ANC.B7.DE30",
+        "display": "Leg cramps",
+        "target": [
+            {
+                "code": "1.56399E+16",
+                "display": "Cramp of muscle of bilateral lower limbs (finding)",
+                "equivalence": "relatedto",
+            }
+        ],
+    },
+    {
+        "code": "ANC.B7.DE35",
+        "display": "Pain - Leg",
+        "target": [
+            {
+                "code": "1.22475E+16",
+                "display": "Pain in bilateral lower legs (finding)",
+                "equivalence": "relatedto",
+            }
+        ],
+    },
+    {
+        "code": "ANC.B7.DE56",
+        "display": "Leg cramps",
+        "target": [
+            {
+                "code": "1.56399E+16",
+                "display": "Cramp of muscle of bilateral lower limbs (finding)",
+                "equivalence": "relatedto",
+            }
+        ],
+    },
+    {
+        "code": "ANC.B7.DE61",
+        "display": "Pain - Leg",
+        "target": [
+            {
+                "code": "1.22475E+16",
+                "display": "Pain in bilateral lower legs (finding)",
+                "equivalence": "relatedto",
+            }
+        ],
+    },
+    {
+        "code": "ANC.B8.DE90",
+        "display": "Mucopurulent cervicitis",
+        "target": [
+            {
+                "code": "1.07437E+16",
+                "display": "Inflammation of cervix in pregnancy (disorder)",
+                "equivalence": "relatedto",
+            }
+        ],
+    },
+    {
+        "code": "ANC.End.17",
+        "display": "Preterm Birth",
+        "target": [
+            {
+                "code": "1.07612E+16",
+                "display": "Preterm labour with preterm delivery (finding)",
+                "equivalence": "equivalent",
+            }
+        ],
+    },
+    {
+        "code": "ANC.B10.DE61",
+        "display": "Counselling conducted on healthy eating and keeping physically active",
+        "target": [
+            {
+                "code": "4.41041E+14",
+                "display": "Counseling about nutrition (procedure)",
+                "equivalence": "equivalent",
+            }
+        ],
+    },
+    {
+        "code": "ANC.B10.DE61",
+        "display": "Counselling conducted on healthy eating and keeping physically active",
+        "target": [
+            {
+                "code": "4.41041E+14",
+                "display": "Counseling about nutrition (procedure)",
+                "equivalence": "equivalent",
+            }
+        ],
+    },
+    {
+        "code": "ANC.B10.DE69",
+        "display": "Counselling conducted on balanced energy and protein dietary supplementation",
+        "target": [
+            {
+                "code": "4.41041E+14",
+                "display": "Counseling about nutrition (procedure)",
+                "equivalence": "equivalent",
+            }
+        ],
+    },
+]
